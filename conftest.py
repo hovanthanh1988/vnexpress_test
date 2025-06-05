e25049c51c678199c1e99eac1fddce979a6c5c1e
hovanthanh1988 <luckylucke.thanh@gmail.com>
2025-06-05 14:03:13 +0700

    v6

--- conftest.py
+++ conftest.py
@@ -1,9 +1,20 @@
+import tempfile
+
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
 
 @pytest.fixture(scope="function")
 def setup(request):
-    driver = webdriver.Chrome()
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")  # Run in headless mode
+    chrome_options.add_argument("--no-sandbox")  # Needed for CI
+    chrome_options.add_argument("--disable-dev-shm-usage")  # Overcome limited resource problems
+    chrome_options.add_argument("--disable-gpu")  # Disable GPU acceleration (safe)
+    chrome_options.add_argument(f"--user-data-dir={tempfile.mkdtemp()}")  # Unique temp profile
+
+    driver = webdriver.Chrome(options=chrome_options)
     driver.maximize_window()
     request.cls.driver = driver
     yield
