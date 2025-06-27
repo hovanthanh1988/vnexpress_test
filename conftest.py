ba543d466cfd9c3aded3d4bedfd77acc6fdc5e42
hovanthanh1988 <luckylucke.thanh@gmail.com>
2025-06-27 07:40:18 +0700

    v8

--- conftest.py
+++ conftest.py
@@ -3,11 +3,12 @@ import tempfile
 
 import pytest
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.options import Options as ChromeOptions
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.firefox import GeckoDriverManager
 from webdriver_manager.microsoft import EdgeChromiumDriverManager
+from selenium.webdriver.chrome.service import Service as ChromeService
 
 driver = None
 @pytest.fixture(scope="function")
@@ -15,8 +16,13 @@ def setup(request):
     browser = request.config.getoption("--browser")
 
     if browser == "chrome":
-        service = Service(ChromeDriverManager().install())
-        driver = webdriver.Chrome(service=service)
+        options = ChromeOptions()
+        options.add_argument("--headless")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
+        options.add_argument("--disable-gpu")
+        service = ChromeService(ChromeDriverManager().install())
+        driver = webdriver.Chrome(service=service, options=options)
     elif browser == "firefox":
         service = Service(GeckoDriverManager().install())
         driver = webdriver.Firefox(service=service)
